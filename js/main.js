dad125843ade2a4094f9a2947243e1896a460b30
Ivasska <113020143+Ivasska@users.noreply.github.com>
2022-10-10 19:21:27 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-btn');
+const navBtnImg = document.querySelector('#nav-btn-img');
+
+navBtn.onclick = () => {
+	if (nav.classList.toggle('open')) {
+		navBtnImg.src = "./img/icons/nav-close.svg";
+	} else {
+		navBtnImg.src = "./img/icons/nav-open.svg";
+	}
+}
+
+AOS.init({
+	disable: 'phone', /* Відключає анімацію на телефонах */
+	once: true /* Анімація лише один раз показується */
+});
